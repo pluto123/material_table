116530168bd7786ff990152c0a1c98ad1fbc34e5
Yuh-Chang Wu <yuh.chang.wu@gmail.com>
2022-01-15 22:19:18 +0800

    不輸出欄位 Age

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ function App() {
     { title: "Name", field: "name", sorting: true, customSort: (a, b) => a.name.length - b.name.length, filtering: false },
     { title: "Email", field: "email", filterPlaceholder: "Filter by email" },
     { title: "Phone Number", field: "phone"/*, align: 'right' */}, // align: [right | center | left]
-    { title: "Age", field: "age", emptyValue: ()=><em>null</em>, searchable: false/*. defaultSort: "asc"*/},
+    { title: "Age", field: "age", emptyValue: ()=><em>null</em>, searchable: false, export: false/*. defaultSort: "asc"*/},
     { title: "Gender", field: "gender", lookup: genderLookup },
     { title: "City", field: "city" },
     { title: "School Fee", field: "fee", type: "currency", currencySetting: { currencyCode: "TWD", minimumFractionDigits: 1 } }
