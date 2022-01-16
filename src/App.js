9173d1cd899639666c180a77c0603a504bf64c54
Yuh-Chang Wu <yuh.chang.wu@gmail.com>
2022-01-16 12:52:04 +0800

    讓某欄位不可被分組

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ function App() {
   const columns = [
     { title: "Name", field: "name", sorting: true, customSort: (a, b) => a.name.length - b.name.length, filtering: false },
     { title: "Email", field: "email", filterPlaceholder: "Filter by email" },
-    { title: "Phone Number", field: "phone"/*, align: 'right' */}, // align: [right | center | left]
+    { title: "Phone Number", field: "phone", grouping: false/*, align: 'right' */}, // align: [right | center | left]
     { title: "Age", field: "age", defaultGroupOrder:1, emptyValue: ()=><em>null</em>, searchable: false, export: false/*. defaultSort: "asc"*/},
     { title: "Gender", field: "gender", defaultGroupOrder:0, lookup: genderLookup },
     { title: "City", field: "city" },
